4aa895a2bcc73cced91f59f43f336dbeebee4b5b
BetoNajera <ing.betonajera@gmail.com>
2024-04-30 16:49:20 -0600

    :sparkles: Feat: Create common module
    Create common module

---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,14 @@
+import { IsPositive, IsOptional, Min } from "class-validator"
+import { Type } from "class-transformer"
+
+export class PaginationDto {
+  @Type(() => Number)
+  @IsOptional()
+  @IsPositive()
+  limit?: number
+
+  @Type(() => Number)
+  @IsOptional()
+  @Min(0)
+  offset?: number
+}
